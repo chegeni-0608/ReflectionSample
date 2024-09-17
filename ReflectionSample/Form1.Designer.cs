1fb4bd39ad7ebbc6190f3f886dcdfb8b0dc004e6
chegeni-0608 <mmoa80610@gmail.com>
2024-09-17 12:28:33 +0330

    Add project files.

---
+++ ReflectionSample/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ReflectionSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetClassListWithReflection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetClassListWithReflection
+            // 
+            this.btnGetClassListWithReflection.Location = new System.Drawing.Point(12, 21);
+            this.btnGetClassListWithReflection.Name = "btnGetClassListWithReflection";
+            this.btnGetClassListWithReflection.Size = new System.Drawing.Size(205, 32);
+            this.btnGetClassListWithReflection.TabIndex = 0;
+            this.btnGetClassListWithReflection.Text = "Get Class List With Reflection";
+            this.btnGetClassListWithReflection.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetClassListWithReflection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetClassListWithReflection;
+    }
+}
+
---
+++ ReflectionSample/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionSample.Models
+{
+     class CategoryModel
+    {
+        public int CatrgoryId { get; set; }
+        public string Title { get; set; }
+    }
+}
